af5648462a881652e7535ae94b255b8b6919e186
Eduardo Mariano <eduardo.m.vasconcelos@eaportal.org>
2023-09-13 08:38:38 -0300

    final

--- revisao_while.py
+++ revisao_while.py
@@ -1,5 +1,21 @@
-contador = 1
+#while - laço de repetição
+#3 parametros: contador, condição, atualizar o contador
 
-while contador <= 10:
+#Exemplo1 - mostrar os números de 1 até um número informado
+
+#numero = int(input("informe um numero inteiro: "))
+
+#contador = 1
+
+#while contador <= numero:
+#    print(contador)
+#    contador += 1
+
+#Exenplo 2 - mostrar o numero de pares de 0 até o numero informado
+
+numero = int(input("informe um numero inteiro: "))
+contador = 0 
+
+while contador <= numero:
     print(contador)
-    contador +=1
+    contador += 2
